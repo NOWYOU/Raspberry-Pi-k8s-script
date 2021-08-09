a296d0c208754cb62c9f79154fc30b02c9f29e32
NOWYOU <43359644+NOWYOU@users.noreply.github.com>
2021-08-09 17:24:23 +0900

    Add files via upload

---
+++ 4.worker.sh
@@ -0,0 +1,515 @@
+#!/bin/bash
+#
+echo ""
+echo "---------------------------依存パッケージのインストール---------------------------"
+echo ""
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "apt-get install -y chrony conntrack ipvsadm ipset jq iptables curl sysstat libseccomp-dev wget socat git" &
+  done
+
+
+echo ""
+echo "---------------------------apiserver 高可用,基于 nginx 代理的 kube-apiserver 高可用方案---------------------------"
+echo ""
+#nginxのダウンロードとコンパイル
+cd /opt/k8s/work
+wget http://nginx.org/download/nginx-1.18.0.tar.gz
+tar -xzvf nginx-1.18.0.tar.gz
+
+cd /opt/k8s/work/nginx-1.18.0
+mkdir nginx-prefix
+apt-get install -y gcc make
+./configure --with-stream --without-http --prefix=$(pwd)/nginx-prefix --without-http_uwsgi_module --without-http_scgi_module --without-http_fastcgi_module
+
+cd /opt/k8s/work/nginx-1.18.0
+make && make install
+
+#nginxのインストールとデプロイ
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p /opt/k8s/kube-nginx/{conf,logs,sbin}"
+  done
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p /opt/k8s/kube-nginx/{conf,logs,sbin}"
+    scp /opt/k8s/work/nginx-1.18.0/nginx-prefix/sbin/nginx  root@${node_ip}:/opt/k8s/kube-nginx/sbin/kube-nginx
+    ssh root@${node_ip} "chmod a+x /opt/k8s/kube-nginx/sbin/*"
+  done  
+
+#Nginx４層透過プロキシフォワーディングができるようにnginxを設定する 
+cd /opt/k8s/work
+cat > kube-nginx.conf << \EOF
+worker_processes 1;
+
+events {
+    worker_connections  1024;
+}
+
+stream {
+    upstream backend {
+        hash $remote_addr consistent;
+        server 192.168.11.30:6443        max_fails=3 fail_timeout=30s;
+        server 192.168.11.31:6443        max_fails=3 fail_timeout=30s;
+    }
+
+    server {
+        listen 127.0.0.1:8443;
+        proxy_connect_timeout 1s;
+        proxy_pass backend;
+    }
+}
+EOF
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    scp kube-nginx.conf  root@${node_ip}:/opt/k8s/kube-nginx/conf/kube-nginx.conf
+  done
+
+#systemdユニットファイルの設定とサービスの開始
+cd /opt/k8s/work
+cat > kube-nginx.service <<EOF
+[Unit]
+Description=kube-apiserver nginx proxy
+After=network.target
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+ExecStartPre=/opt/k8s/kube-nginx/sbin/kube-nginx -c /opt/k8s/kube-nginx/conf/kube-nginx.conf -p /opt/k8s/kube-nginx -t
+ExecStart=/opt/k8s/kube-nginx/sbin/kube-nginx -c /opt/k8s/kube-nginx/conf/kube-nginx.conf -p /opt/k8s/kube-nginx
+ExecReload=/opt/k8s/kube-nginx/sbin/kube-nginx -c /opt/k8s/kube-nginx/conf/kube-nginx.conf -p /opt/k8s/kube-nginx -s reload
+PrivateTmp=true
+Restart=always
+RestartSec=5
+StartLimitInterval=0
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    scp kube-nginx.service  root@${node_ip}:/etc/systemd/system/
+  done
+
+#kube-nginxサービスの開始
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "systemctl daemon-reload && systemctl enable kube-nginx && systemctl restart kube-nginx"
+  done
+
+echo ""
+echo "---------------------------kubeletコンポーネントのデプロイ---------------------------"
+echo ""
+#　kubelet bootstrap kubeconfig ファイルの作成
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_name in ${NODE_NAMES[@]}
+  do
+    echo ">>> ${node_name}"
+
+    # トークンの作成
+    export BOOTSTRAP_TOKEN=$(kubeadm token create \
+      --description kubelet-bootstrap-token \
+      --groups system:bootstrappers:${node_name} \
+      --kubeconfig ~/.kube/config)
+
+    # クラスターパラメーターの設定
+    kubectl config set-cluster kubernetes \
+      --certificate-authority=/etc/kubernetes/cert/ca.pem \
+      --embed-certs=true \
+      --server=${KUBE_APISERVER} \
+      --kubeconfig=kubelet-bootstrap-${node_name}.kubeconfig
+
+    # クライアント認証パラメータの設定
+    kubectl config set-credentials kubelet-bootstrap \
+      --token=${BOOTSTRAP_TOKEN} \
+      --kubeconfig=kubelet-bootstrap-${node_name}.kubeconfig
+
+    # コンテクストパラメータの設定
+    kubectl config set-context default \
+      --cluster=kubernetes \
+      --user=kubelet-bootstrap \
+      --kubeconfig=kubelet-bootstrap-${node_name}.kubeconfig
+
+    # デフォルトコンテキストの設定
+    kubectl config use-context default --kubeconfig=kubelet-bootstrap-${node_name}.kubeconfig
+  done
+
+kubeadm token list --kubeconfig ~/.kube/config
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_name in ${NODE_NAMES[@]}
+  do
+    echo ">>> ${node_name}"
+    scp kubelet-bootstrap-${node_name}.kubeconfig root@${node_name}:/etc/kubernetes/kubelet-bootstrap.kubeconfig
+  done
+
+#kubeletパラメータプロファイルテンプレートの作成
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+cat > kubelet-config.yaml.template <<EOF
+kind: KubeletConfiguration
+apiVersion: kubelet.config.k8s.io/v1beta1
+address: "##NODE_IP##"
+staticPodPath: ""
+syncFrequency: 1m
+fileCheckFrequency: 20s
+httpCheckFrequency: 20s
+staticPodURL: ""
+port: 10250
+readOnlyPort: 0
+rotateCertificates: true
+serverTLSBootstrap: true
+authentication:
+  anonymous:
+    enabled: false
+  webhook:
+    enabled: true
+  x509:
+    clientCAFile: "/etc/kubernetes/cert/ca.pem"
+authorization:
+  mode: Webhook
+registryPullQPS: 0
+registryBurst: 20
+eventRecordQPS: 0
+eventBurst: 20
+enableDebuggingHandlers: true
+enableContentionProfiling: true
+healthzPort: 10248
+healthzBindAddress: "##NODE_IP##"
+clusterDomain: "${CLUSTER_DNS_DOMAIN}"
+clusterDNS:
+  - "${CLUSTER_DNS_SVC_IP}"
+nodeStatusUpdateFrequency: 10s
+nodeStatusReportFrequency: 1m
+imageMinimumGCAge: 2m
+imageGCHighThresholdPercent: 85
+imageGCLowThresholdPercent: 80
+volumeStatsAggPeriod: 1m
+kubeletCgroups: ""
+systemCgroups: ""
+cgroupRoot: ""
+cgroupsPerQOS: true
+cgroupDriver: cgroupfs
+runtimeRequestTimeout: 10m
+hairpinMode: promiscuous-bridge
+maxPods: 220
+podCIDR: "${CLUSTER_CIDR}"
+podPidsLimit: -1
+resolvConf: /etc/resolv.conf
+maxOpenFiles: 1000000
+kubeAPIQPS: 1000
+kubeAPIBurst: 2000
+serializeImagePulls: false
+evictionHard:
+  memory.available:  "100Mi"
+  nodefs.available:  "10%"
+  nodefs.inodesFree: "5%"
+  imagefs.available: "15%"
+evictionSoft: {}
+enableControllerAttachDetach: true
+failSwapOn: true
+containerLogMaxSize: 20Mi
+containerLogMaxFiles: 10
+systemReserved: {}
+kubeReserved: {}
+systemReservedCgroup: ""
+kubeReservedCgroup: ""
+enforceNodeAllocatable: ["pods"]
+EOF
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do 
+    echo ">>> ${node_ip}"
+    sed -e "s/##NODE_IP##/${node_ip}/" kubelet-config.yaml.template > kubelet-config-${node_ip}.yaml.template
+    scp kubelet-config-${node_ip}.yaml.template root@${node_ip}:/etc/kubernetes/kubelet-config.yaml
+  done
+
+#kubelet systemd unit ファイルの作成と配布
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+cat > kubelet.service.template <<EOF
+[Unit]
+Description=Kubernetes Kubelet
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+After=containerd.service
+Requires=containerd.service
+
+[Service]
+WorkingDirectory=${K8S_DIR}/kubelet
+ExecStart=/opt/k8s/bin/kubelet \\
+  --bootstrap-kubeconfig=/etc/kubernetes/kubelet-bootstrap.kubeconfig \\
+  --cert-dir=/etc/kubernetes/cert \\
+  --root-dir=${K8S_DIR}/kubelet \\
+  --kubeconfig=/etc/kubernetes/kubelet.kubeconfig \\
+  --config=/etc/kubernetes/kubelet-config.yaml \\
+  --hostname-override=##NODE_NAME## \\
+  --image-pull-progress-deadline=15m \\
+  --volume-plugin-dir=${K8S_DIR}/kubelet/kubelet-plugins/volume/exec/ \\
+  --logtostderr=true \\
+  --v=2
+Restart=always
+RestartSec=5
+StartLimitInterval=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_name in ${NODE_NAMES[@]}
+  do 
+    echo ">>> ${node_name}"
+    sed -e "s/##NODE_NAME##/${node_name}/" kubelet.service.template > kubelet-${node_name}.service
+    scp kubelet-${node_name}.service root@${node_name}:/etc/systemd/system/kubelet.service
+  done
+
+kubectl create clusterrolebinding kube-apiserver:kubelet-apis --clusterrole=system:kubelet-api-admin --user kubernetes-master
+kubectl create clusterrolebinding kubelet-bootstrap --clusterrole=system:node-bootstrapper --group=system:bootstrappers
+
+#CSRリクエストの自動承認とkubeletクライアント証明書の生成
+cd /opt/k8s/work
+cat > csr-crb.yaml <<EOF
+ # Approve all CSRs for the group "system:bootstrappers"
+ kind: ClusterRoleBinding
+ apiVersion: rbac.authorization.k8s.io/v1
+ metadata:
+   name: auto-approve-csrs-for-group
+ subjects:
+ - kind: Group
+   name: system:bootstrappers
+   apiGroup: rbac.authorization.k8s.io
+ roleRef:
+   kind: ClusterRole
+   name: system:certificates.k8s.io:certificatesigningrequests:nodeclient
+   apiGroup: rbac.authorization.k8s.io
+---
+ # To let a node of the group "system:nodes" renew its own credentials
+ kind: ClusterRoleBinding
+ apiVersion: rbac.authorization.k8s.io/v1
+ metadata:
+   name: node-client-cert-renewal
+ subjects:
+ - kind: Group
+   name: system:nodes
+   apiGroup: rbac.authorization.k8s.io
+ roleRef:
+   kind: ClusterRole
+   name: system:certificates.k8s.io:certificatesigningrequests:selfnodeclient
+   apiGroup: rbac.authorization.k8s.io
+---
+# A ClusterRole which instructs the CSR approver to approve a node requesting a
+# serving cert matching its client cert.
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: approve-node-server-renewal-csr
+rules:
+- apiGroups: ["certificates.k8s.io"]
+  resources: ["certificatesigningrequests/selfnodeserver"]
+  verbs: ["create"]
+---
+ # To let a node of the group "system:nodes" renew its own server credentials
+ kind: ClusterRoleBinding
+ apiVersion: rbac.authorization.k8s.io/v1
+ metadata:
+   name: node-server-cert-renewal
+ subjects:
+ - kind: Group
+   name: system:nodes
+   apiGroup: rbac.authorization.k8s.io
+ roleRef:
+   kind: ClusterRole
+   name: approve-node-server-renewal-csr
+   apiGroup: rbac.authorization.k8s.io
+EOF
+kubectl apply -f csr-crb.yaml
+
+#kubeletサービスの開始
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p ${K8S_DIR}/kubelet/kubelet-plugins/volume/exec/"
+    ssh root@${node_ip} "/usr/sbin/swapoff -a"
+    ssh root@${node_ip} "systemctl daemon-reload && systemctl enable kubelet && systemctl restart kubelet"
+  done
+
+#サーバー証明書csrを手動で承認する
+kubectl get csr
+kubectl get csr | grep Pending | awk '{print $1}' | xargs kubectl certificate approve
+
+#证书认证和授权
+curl -s --cacert /etc/kubernetes/cert/ca.pem --cert /etc/kubernetes/cert/kube-controller-manager.pem --key /etc/kubernetes/cert/kube-controller-manager-key.pem https://192.168.11.30:10250/metrics 
+curl -s --cacert /etc/kubernetes/cert/ca.pem --cert /opt/k8s/work/admin.pem --key /opt/k8s/work/admin-key.pem https://192.168.11.30:10250/metrics|head
+
+#ベアトークンの認証と承認
+kubectl create sa kubelet-api-test
+kubectl create clusterrolebinding kubelet-api-test --clusterrole=system:kubelet-api-admin --serviceaccount=default:kubelet-api-test
+SECRET=$(kubectl get secrets | grep kubelet-api-test | awk '{print $1}')
+TOKEN=$(kubectl describe secret ${SECRET} | grep -E '^token' | awk '{print $2}')
+echo ${TOKEN}
+
+curl -s --cacert /etc/kubernetes/cert/ca.pem -H "Authorization: Bearer ${TOKEN}" https://192.168.11.30:10250/metrics | head
+
+echo ""
+echo "部署 kube-proxy 组件"
+echo ""
+#kube-proxy証明書を作成する
+cd /opt/k8s/work
+cat > kube-proxy-csr.json <<EOF
+{
+  "CN": "system:kube-proxy",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "JP",
+      "ST": "Kyoto",
+      "L": "Kyoto",
+      "O": "k8s",
+      "OU": "KCGI"
+    }
+  ]
+}
+EOF
+
+cd /opt/k8s/work
+cfssl gencert -ca=/opt/k8s/work/ca.pem \
+  -ca-key=/opt/k8s/work/ca-key.pem \
+  -config=/opt/k8s/work/ca-config.json \
+  -profile=kubernetes  kube-proxy-csr.json | cfssljson -bare kube-proxy
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+kubectl config set-cluster kubernetes \
+  --certificate-authority=/opt/k8s/work/ca.pem \
+  --embed-certs=true \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials kube-proxy \
+  --client-certificate=kube-proxy.pem \
+  --client-key=kube-proxy-key.pem \
+  --embed-certs=true \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes \
+  --user=kube-proxy \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_name in ${NODE_NAMES[@]}
+  do
+    echo ">>> ${node_name}"
+    scp kube-proxy.kubeconfig root@${node_name}:/etc/kubernetes/
+  done
+
+#kube-proxy構成ファイルを作成する
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+cat > kube-proxy-config.yaml.template <<EOF
+kind: KubeProxyConfiguration
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+clientConnection:
+  burst: 200
+  kubeconfig: "/etc/kubernetes/kube-proxy.kubeconfig"
+  qps: 100
+bindAddress: ##NODE_IP##
+healthzBindAddress: ##NODE_IP##:10256
+metricsBindAddress: ##NODE_IP##:10249
+enableProfiling: true
+clusterCIDR: ${CLUSTER_CIDR}
+hostnameOverride: ##NODE_NAME##
+mode: "ipvs"
+portRange: ""
+iptables:
+  masqueradeAll: false
+ipvs:
+  scheduler: rr
+  excludeCIDRs: []
+EOF
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for (( i=0; i < 2; i++ ))
+  do 
+    echo ">>> ${NODE_NAMES[i]}"
+    sed -e "s/##NODE_NAME##/${NODE_NAMES[i]}/" -e "s/##NODE_IP##/${NODE_IPS[i]}/" kube-proxy-config.yaml.template > kube-proxy-config-${NODE_NAMES[i]}.yaml.template
+    scp kube-proxy-config-${NODE_NAMES[i]}.yaml.template root@${NODE_NAMES[i]}:/etc/kubernetes/kube-proxy-config.yaml
+  done
+
+# kube-proxy systemd unit の作成と配布
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+cat > kube-proxy.service <<EOF
+[Unit]
+Description=Kubernetes Kube-Proxy Server
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+After=network.target
+
+[Service]
+WorkingDirectory=${K8S_DIR}/kube-proxy
+ExecStart=/opt/k8s/bin/kube-proxy \\
+  --config=/etc/kubernetes/kube-proxy-config.yaml \\
+  --logtostderr=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_name in ${NODE_NAMES[@]}
+  do 
+    echo ">>> ${node_name}"
+    scp kube-proxy.service root@${node_name}:/etc/systemd/system/
+  done
+
+#kube-proxyサービスの開始
+cd /opt/k8s/work
+source /opt/k8s/bin/environment.sh
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p ${K8S_DIR}/kube-proxy"
+    ssh root@${node_ip} "modprobe ip_vs_rr"
+    ssh root@${node_ip} "systemctl daemon-reload && systemctl enable kube-proxy && systemctl restart kube-proxy"
+  done
+
